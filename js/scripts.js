a6ae5fc7eb0fdda7d19000e997a24a5a06defe0a
ramtinteymouri <ramtinteymouri@gmail.com>
2024-04-09 20:46:11 -0700

    added script js file and linked to contacts

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+(function () {
+    let form = document.querySelector('#contact-form');
+    let emailInput = document.querySelector('#contact-email');
+    // TODO: need to add field validations
+
+})()
